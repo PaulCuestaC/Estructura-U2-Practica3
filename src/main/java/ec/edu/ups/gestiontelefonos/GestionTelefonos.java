475f9b86af9eee97cae93e2ccfbf8ed8762faaa0
PaulCuestaC <paulcuestacuzme@gmail.com>
2023-07-17 12:57:59 -0500

    UPD: Contacto,Main. ADD:ArbolContacto

--- src/main/java/ec/edu/ups/gestiontelefonos/GestionTelefonos.java
+++ src/main/java/ec/edu/ups/gestiontelefonos/GestionTelefonos.java
@@ -1,16 +1,31 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-
 package ec.edu.ups.gestiontelefonos;
+
+import ec.edu.ups.gestiontelefonos.controlador.ArbolContactos;
+import ec.edu.ups.gestiontelefonos.modelo.Contacto;
 
 /**
  *
  * @author ESTUDIANTE
  */
 public class GestionTelefonos {
-
+    
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        ArbolContactos arbol = new ArbolContactos();
+        
+        Contacto c1 = new Contacto("Juan", "12345");
+        Contacto c2 = new Contacto("Maria", "12345");
+        Contacto c3 = new Contacto("Pedro", "12345");
+        Contacto c4 = new Contacto("Pablo", "12345");
+        
+        arbol.insert(c1);
+        arbol.insert(c2);
+        arbol.insert(c3);
+        arbol.insert(c4);
+        arbol.insert(new Contacto("Julina", "44857"));
+        arbol.insert(new Contacto("Jhon", "35483749"));
+        
     }
 }
--- src/main/java/ec/edu/ups/gestiontelefonos/controlador/ArbolContactos.java
+++ src/main/java/ec/edu/ups/gestiontelefonos/controlador/ArbolContactos.java
@@ -2,12 +2,88 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package ec.edu.ups.gestiontelefonos.modelo;
+package ec.edu.ups.gestiontelefonos.controlador;
+
+import ec.edu.ups.gestiontelefonos.modelo.Contacto;
+import ec.edu.ups.gestiontelefonos.modelo.Nodo;
+import java.util.Scanner;
+import javax.swing.JOptionPane;
 
 /**
  *
  * @author ESTUDIANTE
  */
 public class ArbolContactos {
-   
+
+    private Nodo raiz;
+    Scanner sc = new Scanner(System.in);
+
+    public ArbolContactos() {
+        this.raiz = null;
+    }
+
+    public void insert(Contacto contacto) {
+        if (raiz == null) {
+            raiz = new Nodo(contacto);
+        } else {
+            insertRecursivo(raiz, contacto);
+        }
+    }
+
+    private void insertRecursivo(Nodo nodo, Contacto contacto) {
+        if (contacto.getNombre().compareTo(nodo.getContacto().getNombre()) < 0) {
+            if (nodo.getLeft() == null) {
+                nodo.setLeft(nodo);
+            } else {
+                insertRecursivo(nodo.getLeft(), contacto);
+            }
+        } else if (contacto.getNombre().compareTo(nodo.getContacto().getNombre()) > 0) {
+            if (nodo.getRight() == null) {
+                nodo.setRight(nodo);
+            } else {
+                insertRecursivo(nodo.getRight(), contacto);
+            }
+
+        } else {
+            System.out.println("¿Desea actualizar el numero?");
+            System.out.println("1 = Si");
+            System.out.println("2 = No");
+            int scanner = sc.nextInt();
+            if (scanner == 1) {
+                nodo.setContacto(contacto);
+            }
+        }
+
+    }
+
+    public boolean setEquilibradi() {
+        return verificarBalance(raiz);
+    }
+
+    private boolean verificarBalance(Nodo node) {
+        if (node == null) {
+            return true;
+        } else {
+
+            int alturaIzquierda = obtenerAltura(node.getLeft());
+            int alturaDerecha = obtenerAltura(node.getRight());
+            int diferencia = Math.abs(alturaIzquierda - alturaDerecha);
+
+            if (diferencia > 1) {
+                return false;
+            }
+            return verificarBalance(node.getLeft()) && verificarBalance(node.getRight());
+        }
+    }
+
+    private int obtenerAltura(Nodo nodo) {
+        if (nodo == null) {
+            return 1;
+        }
+        int alturaIzquierda = obtenerAltura(nodo.getLeft());
+        int alturaDerecha = obtenerAltura(nodo.getRight());
+
+        return Math.max(alturaIzquierda, alturaDerecha + 1);
+
+    }
 }
